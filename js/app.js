0065167903805580f5f4850a623e88f1825af602
benjamingohps <benjamingohps@gmail.com>
2024-10-01 13:21:24 +0800

    Step 2 done

--- js/app.js
+++ js/app.js
@@ -2,8 +2,24 @@
 
 /*---------------------------- Variables (state) ----------------------------*/
 
+let board = "";
+let turn = "";
+let winner = "";
+let tie = "";
 /*------------------------ Cached Element References ------------------------*/
 
+const squareEls = document.querySelector(".board");
+
+const messageEl = document.querySelector("#message");
+
+console.log(squareEls);
+console.log(messageEl);
 /*-------------------------------- Functions --------------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+// const squareEls = document
+//   .querySelector(".sqr")
+//   .addEventListener("click", (event) => {
+//     // board = event.target.id;
+//     console.log(event.target.id);
+//   });
