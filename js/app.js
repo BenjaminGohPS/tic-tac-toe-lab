9eec5c88a7e514570f985aa41ae49148165f2d61
benjamingohps <benjamingohps@gmail.com>
2024-10-01 16:47:26 +0800

    updateBoard works!

--- js/app.js
+++ js/app.js
@@ -26,11 +26,12 @@ const messageEl = document.querySelector("#message");
 console.log(squareEls);
 console.log(messageEl);
 console.log(document.getElementById("1"));
+
 // console.log(board.innerText);
 /*-------------------------------- Functions --------------------------------*/
 
 const init = () => {
-  board = ["X", "X", "", "", "", "", "", "", ""];
+  board = ["O", "X", "", "", "", "O", "", "", "X"];
   turn = "X"; // X - represent player
   winner = "false";
   tie = "false";
@@ -38,10 +39,10 @@ const init = () => {
   console.log(init); // checks
 };
 
-// const render = () => {
-//   updateBoard();
-//   updateMessage();
-// };
+const render = () => {
+  updateBoard();
+  updateMessage();
+};
 
 /* USER STORY
 loop over board
@@ -52,22 +53,24 @@ in the squareEls
 earlier we know board[0]
 */
 
-// const updateBoard = () => {
-//   //   //   for (const b of board) {
-//   //   // squareEls[b];
-//   //   // console.log(squareEls[b]);
-//   //   for (const b of board) {
-//   //     board.forEach((element) => (squareEls.innerText = "X"));
-//   //   }
-//   //   //   board.forEach((element) => (squareEls.innerText = "X"));
+const updateBoard = () => {
+  //   //   for (const b of board) {
+  //   // squareEls[b];
+  //   // console.log(squareEls[b]);
+  //   for (const b of board) {
+  //     board.forEach((element) => (squareEls.innerText = "X"));
+  //   }
+  //   //   board.forEach((element) => (squareEls.innerText = "X"));
 
-// //   for (let i=0; i < board.length; i++) {}
-// console.log()
-// board.forEach((b) => {
-//     squareEls.(b)
-// })
-
-// };
+  for (let i = 0; i < board.length; i++) {
+    if (board[i] === "X") {
+      document.getElementById(`${i}`).innerText = "X";
+    } else if (board[i] === "O") {
+      document.getElementById(`${i}`).innerText = "O";
+    } else {
+    }
+  }
+};
 
 const updateMessage = () => {
   if (winner === "false" && tie === "false") {
@@ -81,6 +84,7 @@ const updateMessage = () => {
 
 init();
 console.log(`This is in board: ${board}`);
+console.log(board.length);
 
 /*----------------------------- Event Listeners -----------------------------*/
 // const squareEls = document
