67e8e5db6534408052ac595991fab5ff54c21aef
benjamingohps <benjamingohps@gmail.com>
2024-10-01 15:02:08 +0800

    updateMessage works when init() is run.

--- js/app.js
+++ js/app.js
@@ -18,12 +18,34 @@ console.log(messageEl);
 /*-------------------------------- Functions --------------------------------*/
 
 const init = () => {
-  board = ["", "", "", "", "", "", "", "", ""];
+  board = ["X", "X", "", "", "", "", "", "", ""];
   turn = "X"; // X - represent player
   winner = "false";
   tie = "false";
   render();
-  console.log(init);
+  console.log(init); // checks
+};
+
+const render = () => {
+  updateBoard();
+  updateMessage();
+};
+
+const updateBoard = () => {
+  for (const b of board) {
+    squareEls[b];
+    console.log(squareEls[b]);
+  }
+};
+
+const updateMessage = () => {
+  if (winner === "false" && tie === "false") {
+    messageEl.innerText = "Player turn";
+  } else if (winner === "false" && tie === "true") {
+    messageEl.innerText = "We have a tie";
+  } else {
+    messageEl.innerText = "Player have won";
+  }
 };
 
 init();
